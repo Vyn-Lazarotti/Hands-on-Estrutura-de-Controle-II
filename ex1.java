6e365036f583200288547fa207e4f1d0c0e17b34
Vinicius Lazarotti <viniciuslazarotti@gmail.com>
2026-03-23 19:12:55 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,9 @@
+public class ex1 {
+    public static void main(String [] arguments){
+        System.out.println("+'''''+");
+        System.out.println("[| o o |]");
+        System.out.println("|  ^  |");
+        System.out.println("| '-' |");
+        System.out.println("+-----+");
+    }
+}
